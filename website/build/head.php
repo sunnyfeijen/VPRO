247992483a5baced2a391e4ebbf30e387850eee4
aline <aline.swinkels@hotmail.com>
2015-06-01 08:42:07 +0200

    website

--- website/build/head.php
+++ website/build/head.php
@@ -1,31 +1,31 @@
 <head>
 
-  	<meta charset="utf-8">
+    <meta charset="utf-8">
 
-  	<title>Genetechs</title>
-  	<meta name="description" content="Genetechs">
- 	<meta name="author" content="Genetechs">
+    <title>Genetechs</title>
+    <meta name="description" content="Genetechs">
+  <meta name="author" content="Genetechs">
 
- 	<!-- Style -->
-  	<link rel="stylesheet" href="css/style.css">
+  <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
     
-  	<!-- Navigation -->
-  	<link rel="stylesheet" href="css/navigation.css">
+    <!-- Navigation -->
+    <link rel="stylesheet" href="css/navigation.css">
     
     <!-- Font Awesome -->
     <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
 
-	<!-- Jquery -->
-  	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-	
-	<!-- Steal SmoothScroll LOL-->
-	<script src="http://zelda-timeline.com/js/smoothscroll.js"></script>
-	<script type="text/javascript" src="js/smoothscroll.js"></script>
+  <!-- Jquery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  
+  <!-- Steal SmoothScroll LOL-->
+  <script src="http://zelda-timeline.com/js/smoothscroll.js"></script>
+  <script type="text/javascript" src="js/smoothscroll.js"></script>
 
-	<!-- Navigation -->
-	<script type="text/javascript" src="js/navigation.js"></script>
+  <!-- Navigation -->
+  <script type="text/javascript" src="js/navigation.js"></script>
 
-	<!-- lettertype -->
-	<link href='http://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
+  <!-- lettertype -->
+  <link href='http://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
 
 </head>
--- website/index.php
+++ website/index.php
@@ -10,10 +10,24 @@ include 'build/head.php';
   		include 'build/navigatie.php';
   	?>
 </header>
-    <section class="welkom">
-        <div class="textwrap">
-            <h1 class="titel">Welkom</h1>
-            <p>Wij zijn Genetechs. Wanneer u graag een kind wilt, en zeker wilt zijn van bepaalde kenmerken, kunt u bij ons terecht. Wij filteren het DNA van uw kind zoals u dat wilt. Heeft uw partner die mooie blonde krullen die uw kind ook zou moeten hebben? Geen probleem, wij regelen het voor je!<br><br>Heeft u interesse?<br>Kijk dan bij onze producten en neem contact met ons op.</p>
+    <section class="contentwrap">
+    	<div id='fotoslide'></div>
+    	<h3>Maak een afspraak</h3>
+        	<div class='two_paragraphs'>
+                <div class="textwrap">
+                	<div class='paragraph'>
+			  			<h1 class="titel">Welkom</h1>
+			            <p>Wij zijn Genetechs. Wanneer u graag een kind wilt, en zeker wilt zijn van bepaalde kenmerken, kunt u bij ons terecht. 
+			            	Wij filteren het DNA van uw kind zoals u dat wilt. Heeft uw partner die mooie blonde krullen die uw kind ook zou moeten hebben? 
+			            	Geen probleem, wij regelen het voor je!<br><br>Heeft u interesse?<br>Kijk dan bij onze producten en neem contact met ons op.</p>
+		            </div>
+		            <div class='paragraph'>
+			            <h1 class="titel">Over ons</h1>
+			            <p>Wij zijn Genetechs. Wanneer u graag een kind wilt, en zeker wilt zijn van bepaalde kenmerken, kunt u bij ons terecht. 
+			            	Wij filteren het DNA van uw kind zoals u dat wilt. Heeft uw partner die mooie blonde krullen die uw kind ook zou moeten hebben? 
+			            	Geen probleem, wij regelen het voor je!<br><br>Heeft u interesse?<br>Kijk dan bij onze producten en neem contact met ons op.</p>
+	  				</div>
+	  			</div>
 	  	</div>
     </section>
 </body>
