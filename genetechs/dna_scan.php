ade8b51034ab1cc066e863ca58694f5ffd3abf2a
aline <aline.swinkels@hotmail.com>
2015-06-02 16:07:59 +0200

    download en scan dan

--- genetechs/dna_scan.php
+++ genetechs/dna_scan.php
@@ -2,46 +2,98 @@
 	echo "<script type='text/javascript'>";
     echo "$(document).ready(function(){
         		$('.container').hide();
+        		$('#background_dna').hide();
+
+
+        		$('#download_dna').on('click', function(){
+        		$('#background_dna').show();
 
         		$('#dna_scan_stripe_yellow').animate({
 		            height:'-15px', bottom: '-10px'
 		        },6000);
 
-				$('#dna_scan_orange').delay(5000).animate({
+				$('#dna_scan_orange').delay(5500).animate({
 		            opacity:'100'
-		        },6000);
+		        },0);
 
 
 				$('#dna_scan_stripe_orange').delay(5000).animate({
 		            height:'-15px', opacity:'100', bottom: '-10px'
 		        },6000);
 
-				$('#dna_scan_blue_dark').delay(10000).animate({
+				$('#dna_scan_blue_dark').delay(10500).animate({
 		            opacity:'100'
-		        },6000);
+		        },0);
 
 				$('#dna_scan_stripe_blue_dark').delay(10000).animate({
 		            height:'-15px', opacity:'100', bottom: '-10px'
 		        },6000);
 
-				$('#dna_scan_blue_light').delay(15000).animate({
+				$('#dna_scan_blue_light').delay(15500).animate({
 		            opacity:'100'
-		        },6000);
+		        },0);
+	
+
 
 				$('#dna_scan_stripe_blue_light').delay(15000).animate({
 		            height:'-15px', opacity:'100', bottom: '-10px'
 		        },6000);
 
+				$('.dna_scan_stripes').delay(500).animate({
+					border:'0px'
+				})
+
+				
+				
 				$('#dna_available').delay(21500).animate({
 		            opacity:'100'
 		        },1500);
+				
+						$('#first_loading').delay(23000).animate({
+				            opacity:'0'
+				        },100);
+						$('#locus').delay(23000).animate({
+				            opacity:'100'
+				        },1500);
+
+						$('#second_loading').delay(25000).animate({
+			            opacity:'0'
+				        },100);
+						$('#chromosoom').delay(25000).animate({
+				            opacity:'100'
+				        },1500);
+
+						$('#third_loading').delay(27000).animate({
+				            opacity:'0'
+				        },100);
+						$('#dna_kenmerk').delay(27000).animate({
+				            opacity:'100'
+				        },1500);
+
+						$('#fourth_loading').delay(29000).animate({
+			            opacity:'0'
+				        },100);
+						$('#frequentie').delay(29000).animate({
+				            opacity:'100'
+				        },1500);
+
+						$('#continue').delay(30000).animate({
+				            opacity:'100'
+				        },1000);
+
+	
+				});
 
         		$('#continue').on('click', function(){
         			$('.container').show();
+        			$('#dna_scan').hide();
+
         		});
         	});";
     echo "</script>";
 ?>   
+<section id='dna_scan'>
+<a id='download_dna'>download DNA</a>
 <section id='background_dna'>
 <section id='dna_scan_yellow' class='dna_scan'>
 </section>
@@ -61,7 +113,15 @@
 	</section>
 <section id='dna_available'>
 <h1>Calculating available DNA</h1>
+<img src='img/loading.gif' class='loading' id='first_loading'/>
+<img src='img/loading.gif' class='loading' id='second_loading'/>
+<img src='img/loading.gif' class='loading' id='third_loading'/>
+<img src='img/loading.gif' class='loading' id='fourth_loading'/>
+	<?php
+		include('dna_tables.php');
+	?>
 </section>
 
 <a id='continue'>continue</a>
+</section>
 </section>
---
+++ genetechs/dna_tables.php
@@ -0,0 +1,121 @@
+<div id='locus' class='tabellen'>
+		<table>
+			<tr>
+				<th>Locus</th>
+			</tr>
+			<tr>
+				<td>DS2S11338</td>
+			</tr>
+			<tr>
+				<td>D19S4 33</td>
+			</tr>
+			<tr>
+				<td>FGA</td>
+			</tr>
+			<tr>
+				<td>D18S51</td>
+			</tr>
+			<tr>
+				<td>D21S11</td>
+			</tr>
+			<tr>
+				<td>D8S1 179</td>
+			</tr>
+			<tr>
+				<td>D16S539</td>
+			</tr>
+		</table>
+	</div>
+		<div id='chromosoom' class='tabellen'>
+		<table>
+			<tr>
+				<th>Chromosoom</th>
+			</tr>
+			<tr>
+				<td>chromosoom 2</td>
+			</tr>
+			<tr>
+				<td>chromosoom 19</td>
+			</tr>
+			<tr>
+				<td>chromosoom 4</td>
+			</tr>
+			<tr>
+				<td>chromosoom 18</td>
+			</tr>
+			<tr>
+				<td>chromosoom 21</td>
+			</tr>
+			<tr>
+				<td>chromosoom 8</td>
+			</tr>
+			<tr>
+				<td>chromosoom 11</td>
+			</tr>
+			<tr>
+				<td>chromosoom 16</td>
+			</tr>
+		</table>
+	</div>
+		<div id='dna_kenmerk' class='tabellen'>
+		<table>
+			<tr>
+				<th>DNA kenmerk</th>
+			</tr>
+			<tr>
+				<td>19/21</td>
+			</tr>
+			<tr>
+				<td>13/15</td>
+			</tr>
+			<tr>
+				<td>21/25</td>
+			</tr>
+			<tr>
+				<td>15/16</td>
+			</tr>
+			<tr>
+				<td>31/33.2</td>
+			</tr>
+			<tr>
+				<td>9/10</td>
+			</tr>
+			<tr>
+				<td>7/9.3</td>
+			</tr>
+			<tr>
+				<td>11/12</td>
+			</tr>
+		</table>
+	</div>
+		<div id='frequentie' class='tabellen'>
+		<table>
+			<tr>
+				<th>Frequentie</th>
+			</tr>
+			<tr>
+				<td>0.4%</td>
+			</tr>
+			<tr>
+				<td>8.4%</td>
+			</tr>
+			<tr>
+				<td>2.6%</td>
+			</tr>
+			<tr>
+				<td>3.6%</td>
+			</tr>
+			<tr>
+				<td>1.4%</td>
+			</tr>
+			<tr>
+				<td>0.2%</td>
+			</tr>
+			<tr>
+				<td>13.4%</td>
+			</tr>
+			<tr>
+				<td>19.0%</td>
+			</tr>
+		</table>
+	</div>
